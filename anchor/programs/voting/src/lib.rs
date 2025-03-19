9f5ff7f630f6ca5621d02bcb183d15a1499f21f0
Godwin John <gj09042003@gmail.com>
2025-03-19 14:59:49 +0100

    tested the voting and made Poll account mutable to fix the not incrementing bug.

--- anchor/programs/voting/src/lib.rs
+++ anchor/programs/voting/src/lib.rs
@@ -48,7 +48,6 @@ pub struct Vote<'info>
     pub signer: Signer<'info>,
 
     #[account(
-        mut,
         seeds = [poll_id.to_le_bytes().as_ref()],
         bump, 
     )]
@@ -56,6 +55,7 @@ pub struct Vote<'info>
     pub poll: Account<'info, Poll>,
 
     #[account(
+        mut,
         seeds = [poll_id.to_le_bytes().as_ref(), candidate_name.as_bytes()],
         bump, 
     )]
