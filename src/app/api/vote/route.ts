f73e0a16414a5d21262066e272b79e0613b6b68a
Godwin John <gj09042003@gmail.com>
2025-03-20 02:54:44 +0100

    Modified routes file and added metadata

--- src/app/api/vote/route.ts
+++ src/app/api/vote/route.ts
@@ -1,3 +1,26 @@
+import { ActionGetResponse, ACTIONS_CORS_HEADERS } from "@solana/actions"
+
+export const OPTIONS = GET;
+
 export async function GET(request: Request) {
-  return new Response('Hello, from API!')
+  const actionMetadata: ActionGetResponse = {
+    icon: "https://cookieandkate.com/images/2025/01/peanut-butter-recipe.jpg",
+    title: "Vote for your favorite type of peanut butter!",
+    description: "Vote between crunchy and smooth peanut butter!",
+    label: "Vote",
+    links: {
+      actions: [
+        {
+          label: "Vote for Crunchy",
+          href: "/api/vote?candidate=crunchy"
+        },
+        {
+          label: "Vote for Smooth",
+          href: "/api/vote?candidate=smooth"
+        }
+      ]
+    }
+
+  };
+  return Response.json(actionMetadata, { headers: ACTIONS_CORS_HEADERS}); 
 }
